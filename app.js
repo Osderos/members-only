d9a667e9eb7ef41cd01308bb27e19b677cc5c18e
Osderos <barda.george@gmail.com>
2022-03-31 20:49:24 +0300

    deploy to heroku

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ var app = express();
 
 app.use(helmet())
 
-const mongoDB = process.env.DB;
+const dev_db_url = process.env.DB;
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error"));
